572e91d3536124d69ec8b5aaf74672f2b6d17513
Luke <llogan382@gmail.com>
2020-07-21 10:31:43 -0400

    Display meta info on page template

--- single-mdn_social_quiz.php
+++ single-mdn_social_quiz.php
@@ -1,3 +1,13 @@
 <?php
 
 echo "<h2>This is the single quiz page</h2>";
+
+$meta_values = get_post_meta( get_the_ID(), 'mdn_quizzes' )[0];
+
+foreach($meta_values as $meta_value){
+    echo 'This is the value ' . $meta_value['TitleItem'] . '</br>';
+    echo 'This is the value ' . $meta_value['TitleDescription'] . '</br></br>';
+
+};
+
+
