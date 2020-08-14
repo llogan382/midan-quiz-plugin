3be6b1ada45bc8375efe4240a828d2d35e5dad34
Luke <llogan382@gmail.com>
2020-08-14 07:13:54 -0400

    Single question and images work

--- includes/repeater-fields.php
+++ includes/repeater-fields.php
@@ -19,19 +19,19 @@ function wporg_custom_box_html($post)
     $value = get_post_meta($post->ID, '_wporg_meta_key', true);
     ?>
 <label for="ques1">Question</label>
-<input name="ques1[]" class="ques" type="text">
+<input name="ques1[]" class="ques" type="text" value="fav southern dish">
 <label for="answerA">A</label>
-<input name="answerA[]" class="answerA" type="text">
-<input id="wp_custom_attachmentA" name="wp_custom_attachmentA" size="25" type="file" value="" />
+<input name="answerA[]" class="answerA" type="text" value="bbq">
+<input id="wp_custom_attachment" name="wp_custom_attachment" size="25" type="file" value="" />
 <label for="answerB">B</label>
-<input name="answerB[]" class="answerB" type="text">
-<input id="wp_custom_attachmentB" name="wp_custom_attachmentB" size="25" type="file" value="" />
+<input name="answerB[]" class="answerB" type="text" value="collards">
+<input id="wp_custom_attachment" name="wp_custom_attachment" size="25" type="file" value="" />
 <label for="answerC">C</label>
-<input name="answerC[]" class="answerC" type="text">
-<input id="wp_custom_attachmentC" name="wp_custom_attachmentC" size="25" type="file" value="" />
+<input name="answerC[]" class="answerC" type="text" value="grits">
+<input id="wp_custom_attachment" name="wp_custom_attachment" size="25" type="file" value="" />
 <label for="answerD">D</label>
-<input name="answerD[]" class="answerD" type="text">
-<input id="wp_custom_attachmentD" name="wp_custom_attachmentD" size="25" type="file" value="" />
+<input name="answerD[]" class="answerD" type="text" value="sweet tea">
+<input id="wp_custom_attachment" name="wp_custom_attachment" size="25" type="file" value="" />
 <?php
 }
 
@@ -40,7 +40,7 @@ function wporg_save_postdata($post_id){
 
 
 
-    if ( ! empty( $_FILES['wp_custom_attachmentA']['name'] ) ) {
+    if ( ! empty( $_FILES['wp_custom_attachment']['name'] ) ) {
 
         $new = array();
         $ques1 = $_POST['ques1'];
@@ -58,40 +58,36 @@ function wporg_save_postdata($post_id){
 		$arr_file_type = wp_check_filetype( basename( $_FILES['wp_custom_attachmentA']['name'] ) );
 		$uploaded_type = $arr_file_type['type'];
 
-		if ( in_array( $uploaded_type, $supported_types ) ) {
-            $upload1 = wp_upload_bits($_FILES['wp_custom_attachmentA']['name'], null, file_get_contents($_FILES['wp_custom_attachmentA']['tmp_name']));
-			$upload2 = wp_upload_bits($_FILES['wp_custom_attachmentB']['name'], null, file_get_contents($_FILES['wp_custom_attachmentB']['tmp_name']));
-			$upload3 = wp_upload_bits($_FILES['wp_custom_attachmentC']['name'], null, file_get_contents($_FILES['wp_custom_attachmentC']['tmp_name']));
-			$upload4 = wp_upload_bits($_FILES['wp_custom_attachmentD']['name'], null, file_get_contents($_FILES['wp_custom_attachmentD']['tmp_name']));
+
+            $upload1 = wp_upload_bits($_FILES['wp_custom_attachment']['name'], null, file_get_contents($_FILES['wp_custom_attachment']['tmp_name']));
+			$upload2 = wp_upload_bits($_FILES['wp_custom_attachment']['name'], null, file_get_contents($_FILES['wp_custom_attachment']['tmp_name']));
+			$upload3 = wp_upload_bits($_FILES['wp_custom_attachment']['name'], null, file_get_contents($_FILES['wp_custom_attachment']['tmp_name']));
+			$upload4 = wp_upload_bits($_FILES['wp_custom_attachment']['name'], null, file_get_contents($_FILES['wp_custom_attachment']['tmp_name']));
 
 
 			if ( isset( $upload['error'] ) && $upload['error'] != 0 ) {
 				wp_die( 'There was an error uploading your file. The error is: ' . $upload['error'] );
 			} else {
 
-                $new[1]['ques']= $ques1;
-                $new[1]['answA']= $answerA;
-                $new[1]['answB']= $answerB;
-                $new[1]['answC']= $answerC;
-                $new[1]['answD']= $answerD;
+                $new['ques']= $ques1;
+                $new['answA']= $answerA;
+                $new['answB']= $answerB;
+                $new['answC']= $answerC;
+                $new['answD']= $answerD;
 
-                $new[1]['img1']= $upload1;
-                $new[1]['img2']= $upload2;
-                $new[1]['img3']= $upload3;
-                $new[1]['img4']= $upload4;
+                $new['img1']= $upload1;
+                $new['img2']= $upload2;
+                $new['img3']= $upload3;
+                $new['img4']= $upload4;
 
 
-				add_post_meta( $post_id, '_wporg_meta_key', $new[1] );
-				update_post_meta( $post_id, '_wporg_meta_key', $new[1] );
+				add_post_meta( $post_id, '_wporg_meta_key', $new );
+				update_post_meta( $post_id, '_wporg_meta_key', $new );
 			}
-		}
-		else {
-			wp_die( "The file type that you've uploaded is not a PDF." );
 		}
 	}
 
 
     //Here do whathever you want with this $title and $desc.
-}
 
 add_action('save_post', 'wporg_save_postdata');
--- single-mdn_social_quiz.php
+++ single-mdn_social_quiz.php
@@ -3,10 +3,24 @@
 
 function output_all_postmeta() {
 
-	$postmetas = get_post_meta(get_the_ID());
+	$postmetas = get_post_meta(get_the_ID(), '_wporg_meta_key');
 
-    $just_answers = $postmetas['_wporg_meta_key'][0];
-    for
 
-}
+    foreach($postmetas as $meta){
+        // foreach($meta as $x => $val) {
+            echo $meta['ques'][0] . "</br>";
+            echo $meta['answA'][0] . "</br>";
+            echo "<img src=" . $meta['img1']['url'] . "></br>";
+            echo $meta['answB'][0] . "</br>";
+            echo "<img src=" . $meta['img2']['url'] . "></br>";
+            echo $meta['answC'][0] . "</br>";
+            echo "<img src=" . $meta['img3']['url'] . "></br>";
+            echo $meta['answD'][0] . "</br>";
+            echo "<img src=" . $meta['img4']['url'] . "></br>";
+
+
+        //   }
+    }
+ }
+
 output_all_postmeta();
