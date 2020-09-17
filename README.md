875bbfba977e4a3d34b416c508f4bf9f98ec7858
Luke <llogan382@gmail.com>
2020-09-17 12:24:42 -0400

    Erases all files

--- README.md
+++
@@ -1,4 +0,0 @@
-midan-quiz-plugin
-
-How to build a plugin from scratch, by Alessandro Castellini
-https://www.youtube.com/playlist?list=PLriKzYyLb28kR_CPMz8uierDWC2y3znI2
---
+++ lwd_social_quiz.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              loganwebdev.com
+ * @since             1.0.0
+ * @package           lwd_social_quiz
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Logan Social Quiz
+ * Plugin URI:        https://midanmarketing.com
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Midan Dev Team
+ * Author URI:        loganwebdev.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       lwd_social_quiz
+ * Domain Path:       /languages
+ */
+
