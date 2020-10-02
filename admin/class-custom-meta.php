cfd4fc3156fdd0d0cc3a5af38a99e2d34f7cb12b
Luke <llogan382@gmail.com>
2020-10-01 21:33:59 -0400

    Cleanup

--- admin/class-custom-meta.php
+++ admin/class-custom-meta.php
@@ -1,29 +1,4 @@
 <?php
-// class LWDQuiz {
-//     public function hook_into_wordpress() {
-//         add_action( 'add_meta_boxes', [$this, 'wporg_add_custom_box'] );
-//     }
-
-
-
-//     public function wporg_add_custom_box() {
-
-//         require_once(plugin_dir_path(__FILE__).'partials/class-meta-admin-display.php');
-
-
-//         add_meta_box(
-//             'wporg_box_id',           // Unique ID
-//             'Custom Meta Box Title',  // Box title
-//             array( $this, 'showAdminMeta' ),  // Content callback, must be of type callable
-//             'mdn_social_quiz'                   // Post type
-//         );
-//     }
-
-
-
-// }
-
-
 abstract class LWDQuiz
 {
     public static function add()
